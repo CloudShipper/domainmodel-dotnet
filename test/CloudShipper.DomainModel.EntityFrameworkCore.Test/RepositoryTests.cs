f3a65fcac6df76e872b4765519a17d63966f3ee3
Andreas Mock <andreas.mock@live.com>
2022-07-07 17:32:36 +0200

    more repository test cases

--- test/CloudShipper.DomainModel.EntityFrameworkCore.Test/RepositoryTests.cs
+++ test/CloudShipper.DomainModel.EntityFrameworkCore.Test/RepositoryTests.cs
@@ -73,6 +73,8 @@ public class RepositoryTests : IClassFixture<ServiceConfigurationTestFixture>
         Assert.Equal(aggregate, result);
 
         Assert.ThrowsAsync<KeyNotFoundException>(() => repo.GetAsync(id));
+
+        Assert.ThrowsAsync<KeyNotFoundException>(() => repo.GetAsync(Guid.NewGuid()));
     }
 
     [Fact]
@@ -91,5 +93,37 @@ public class RepositoryTests : IClassFixture<ServiceConfigurationTestFixture>
         Assert.Equal(aggregate, result);
 
         Assert.ThrowsAsync<KeyNotFoundException>(() => repo.GetAsync(id));
+    }
+
+    [Fact]
+    public void Test_004_Save()
+    {
+        var id = Guid.NewGuid();
+        var repo = _fixture.ServiceProvider.GetRequiredService<IAggregateRootRepository<DomainObjectA, Guid>>();
+        var factory = _fixture.ServiceProvider.GetRequiredService<IAggregateRootFactory<DomainObjectA, Guid>>();
+
+        var aggregate = factory.Create(id);
+        aggregate.SetValue1(42);
+        aggregate = repo.AddAsync(aggregate).Result;
+        Assert.NotNull(aggregate);
+
+        aggregate.SetValue1(0);
+        repo.SaveAsync(aggregate).Wait();
+
+        var result = repo.GetAsync(id).Result;
+        Assert.NotNull(result);
+        Assert.Equal(0, result.Value1);
+    }
+
+    [Fact]
+    public void Test_005_ThrowArgumentNullException()
+    {
+        var repo = _fixture.ServiceProvider.GetRequiredService<IAggregateRootRepository<DomainObjectA, Guid>>();
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        Assert.ThrowsAsync<ArgumentNullException>(() => repo.AddAsync(null));
+        Assert.ThrowsAsync<ArgumentNullException>(() => repo.DeleteAsync(null));
+        Assert.ThrowsAsync<ArgumentNullException>(() => repo.SaveAsync(null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type
     }
 }
